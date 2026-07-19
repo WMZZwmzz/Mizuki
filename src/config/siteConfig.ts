f72627ab052c5e24a12d5f2e77505135928cb725
WMZZwmzz <wmzz_herry@outlook.com>
2026-07-19 21:33:22 +0800

    chore(content): 通过 keystatic 后台更新内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -9,7 +9,7 @@ export const siteConfig: SiteConfig = {
 	title: keystaticSettings.title || "Mizuki",
 	subtitle: keystaticSettings.subtitle || "One demo website",
 	siteURL: keystaticSettings.siteURL || "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-07-05", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG as SiteConfig["lang"],
 
---
+++ src/content/posts/test.md
@@ -0,0 +1,14 @@
+---
+title: "测试test"
+published: "2026-07-19"
+updated: "2026-07-19"
+description: "文章摘要"
+tags: ["test", "测试"]
+category: "Technology"
+lang: "zh"
+author: "无名之子"
+---
+
+测试测试
+testtest
+这是正文
